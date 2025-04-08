4d93f1d7cee368260c2fdb8984182adaeebc4ed0
Sou800 <nevegivejg818@gmail.com>
2025-04-08 20:42:58 +0900

    Update main.dart

--- lib/main.dart
+++ lib/main.dart
@@ -6,8 +6,8 @@ final con =Container(
   color:Colors.deepPurple,
   width:270,
   height:270,
-  child:Image.asset('images/television.jpg'),
   alignment:Alignment.center,
+   child:Image.asset('images/television.jpg'),
   //padding:EdgeInsets.fromLTRB(10,20,30,40),
 );
 
@@ -15,15 +15,15 @@ final con1=Container(
   color:Colors.redAccent,
   width:270,
   height:270,
-  child:Text('テストダヨ'),
   alignment:Alignment.center,
+   child:Text('テストダヨ'),
 );
 final con2=Container(
   width:270,
   height:270,
   color:Colors.lightGreen,
-  child:Image.asset('images/television.jpg'),
   alignment:Alignment.center,
+   child:Image.asset('images/television.jpg'),
 );
 
 final col =Column(
